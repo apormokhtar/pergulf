3f780386d43a80741e2a7ee4bae5c49994494f59
mokhtar <apormokhtar@gmail.com>
2026-05-09 19:51:29 +0330

    Create renderer\lib\crypto-helper.js

---
+++ renderer\lib\crypto-helper.js
@@ -0,0 +1,7 @@
+// crypto-helper.js
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
